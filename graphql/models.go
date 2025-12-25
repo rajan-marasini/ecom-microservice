4ac81922f72592999045fa598bb8c16c88e0f0b8
Rajan Marasini <official.rajan2060@gmail.com>
2025-12-25 17:43:21 +0545

    feat: Implement GraphQL resolvers for account, product, and order creation and retrieval with pagination and timeouts.

--- graphql/models.go
+++ graphql/models.go
@@ -5,3 +5,15 @@ type Account struct {
 	Name  string  `json:"name"`
 	Order []Order `json:"orders"`
 }
+
+func (p *PaginationInput) Bound() (uint64, uint64) {
+	skip := uint64(0)
+	take := uint64(0)
+	if p.Skip != nil {
+		skip = uint64(*p.Skip)
+	}
+	if p.Take != nil {
+		take = uint64(*p.Take)
+	}
+	return skip, take
+}
--- graphql/mutation_resolver.go
+++ graphql/mutation_resolver.go
@@ -1,19 +1,81 @@
 package main
 
-import "context"
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/rajan-marasini/ecom-microservice/order"
+)
+
+var (
+	ErrInvalidParameter = errors.New("Invalid Parameter")
+)
 
 type mutationResolver struct {
 	server *Server
 }
 
-func (r *mutationResolver) createAccount(ctx context.Context, in AccountInput) (*Account, error) {
-	return nil, nil
+func (r *mutationResolver) CreateAccount(ctx context.Context, in AccountInput) (*Account, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	acc, err := r.server.accountClient.PostAccount(ctx, in.Name)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return &Account{
+		ID:   acc.ID,
+		Name: acc.Name,
+	}, nil
 }
 
-func (r *mutationResolver) createProduct(ctx context.Context, in ProductInput) (*Product, error) {
-	return nil, nil
+func (r *mutationResolver) CreateProduct(ctx context.Context, in ProductInput) (*Product, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	p, err := r.server.catalogClient.PostProduct(ctx, in.Name, in.Description, in.Price)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return &Product{
+		ID:          p.ID,
+		Description: p.Description,
+		Name:        p.Name,
+		Price:       p.Price,
+	}, nil
 }
 
-func (r *mutationResolver) createOrder(ctx context.Context, in OrderInput) (*Order, error) {
-	return nil, nil
+func (r *mutationResolver) CreateOrder(ctx context.Context, in OrderInput) (*Order, error) {
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	var products []order.OrderedProduct
+	for _, p := range in.Products {
+		if p.Quantity <= 0 {
+			return nil, ErrInvalidParameter
+		}
+
+		products = append(products, order.OrderedProduct{
+			ID:       p.ID,
+			Quantity: uint32(p.Quantity),
+		})
+	}
+
+	o, err := r.server.orderClient.PostOrder(ctx, in.AccountID, products)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return &Order{
+		ID:         o.ID,
+		CreatedAt:  o.CreatedAt,
+		TotalPrice: o.TotalPrice,
+	}, nil
 }
